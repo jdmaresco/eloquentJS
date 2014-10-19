6f66f3b8ddf826f2952acaec266881f985e5b6a3
JD Maresco <jdmaresco@gmail.com>
2014-10-18 23:30:08 -0400

    Completed chapter 5 exercises

---
+++ ch5.js
@@ -0,0 +1,200 @@
+// ======================================================
+// Exercises I completed while working through Eloquent Javascript, v2
+// Book: http://eloquentjavascript.net/
+// ======================================================
+
+
+// Utility functions
+function logHeader(title) {
+  console.log("\n\n=====================\n" +
+                title + "\n" +
+              "=====================\n")
+}
+
+// Load data for this chapter
+
+var ANCESTRY_FILE = "[\n  " + [
+  '{"name": "Carolus Haverbeke", "sex": "m", "born": 1832, "died": 1905, "father": "Carel Haverbeke", "mother": "Maria van Brussel"}',
+  '{"name": "Emma de Milliano", "sex": "f", "born": 1876, "died": 1956, "father": "Petrus de Milliano", "mother": "Sophia van Damme"}',
+  '{"name": "Maria de Rycke", "sex": "f", "born": 1683, "died": 1724, "father": "Frederik de Rycke", "mother": "Laurentia van Vlaenderen"}',
+  '{"name": "Jan van Brussel", "sex": "m", "born": 1714, "died": 1748, "father": "Jacobus van Brussel", "mother": "Joanna van Rooten"}',
+  '{"name": "Philibert Haverbeke", "sex": "m", "born": 1907, "died": 1997, "father": "Emile Haverbeke", "mother": "Emma de Milliano"}',
+  '{"name": "Jan Frans van Brussel", "sex": "m", "born": 1761, "died": 1833, "father": "Jacobus Bernardus van Brussel", "mother":null}',
+  '{"name": "Pauwels van Haverbeke", "sex": "m", "born": 1535, "died": 1582, "father": "N. van Haverbeke", "mother":null}',
+  '{"name": "Clara Aernoudts", "sex": "f", "born": 1918, "died": 2012, "father": "Henry Aernoudts", "mother": "Sidonie Coene"}',
+  '{"name": "Emile Haverbeke", "sex": "m", "born": 1877, "died": 1968, "father": "Carolus Haverbeke", "mother": "Maria Sturm"}',
+  '{"name": "Lieven de Causmaecker", "sex": "m", "born": 1696, "died": 1724, "father": "Carel de Causmaecker", "mother": "Joanna Claes"}',
+  '{"name": "Pieter Haverbeke", "sex": "m", "born": 1602, "died": 1642, "father": "Lieven van Haverbeke", "mother":null}',
+  '{"name": "Livina Haverbeke", "sex": "f", "born": 1692, "died": 1743, "father": "Daniel Haverbeke", "mother": "Joanna de Pape"}',
+  '{"name": "Pieter Bernard Haverbeke", "sex": "m", "born": 1695, "died": 1762, "father": "Willem Haverbeke", "mother": "Petronella Wauters"}',
+  '{"name": "Lieven van Haverbeke", "sex": "m", "born": 1570, "died": 1636, "father": "Pauwels van Haverbeke", "mother": "Lievijne Jans"}',
+  '{"name": "Joanna de Causmaecker", "sex": "f", "born": 1762, "died": 1807, "father": "Bernardus de Causmaecker", "mother":null}',
+  '{"name": "Willem Haverbeke", "sex": "m", "born": 1668, "died": 1731, "father": "Lieven Haverbeke", "mother": "Elisabeth Hercke"}',
+  '{"name": "Pieter Antone Haverbeke", "sex": "m", "born": 1753, "died": 1798, "father": "Jan Francies Haverbeke", "mother": "Petronella de Decker"}',
+  '{"name": "Maria van Brussel", "sex": "f", "born": 1801, "died": 1834, "father": "Jan Frans van Brussel", "mother": "Joanna de Causmaecker"}',
+  '{"name": "Angela Haverbeke", "sex": "f", "born": 1728, "died": 1734, "father": "Pieter Bernard Haverbeke", "mother": "Livina de Vrieze"}',
+  '{"name": "Elisabeth Haverbeke", "sex": "f", "born": 1711, "died": 1754, "father": "Jan Haverbeke", "mother": "Maria de Rycke"}',
+  '{"name": "Lievijne Jans", "sex": "f", "born": 1542, "died": 1582, "father":null, "mother":null}',
+  '{"name": "Bernardus de Causmaecker", "sex": "m", "born": 1721, "died": 1789, "father": "Lieven de Causmaecker", "mother": "Livina Haverbeke"}',
+  '{"name": "Jacoba Lammens", "sex": "f", "born": 1699, "died": 1740, "father": "Lieven Lammens", "mother": "Livina de Vrieze"}',
+  '{"name": "Pieter de Decker", "sex": "m", "born": 1705, "died": 1780, "father": "Joos de Decker", "mother": "Petronella van de Steene"}',
+  '{"name": "Joanna de Pape", "sex": "f", "born": 1654, "died": 1723, "father": "Vincent de Pape", "mother": "Petronella Wauters"}',
+  '{"name": "Daniel Haverbeke", "sex": "m", "born": 1652, "died": 1723, "father": "Lieven Haverbeke", "mother": "Elisabeth Hercke"}',
+  '{"name": "Lieven Haverbeke", "sex": "m", "born": 1631, "died": 1676, "father": "Pieter Haverbeke", "mother": "Anna van Hecke"}',
+  '{"name": "Martina de Pape", "sex": "f", "born": 1666, "died": 1727, "father": "Vincent de Pape", "mother": "Petronella Wauters"}',
+  '{"name": "Jan Francies Haverbeke", "sex": "m", "born": 1725, "died": 1779, "father": "Pieter Bernard Haverbeke", "mother": "Livina de Vrieze"}',
+  '{"name": "Maria Haverbeke", "sex": "m", "born": 1905, "died": 1997, "father": "Emile Haverbeke", "mother": "Emma de Milliano"}',
+  '{"name": "Petronella de Decker", "sex": "f", "born": 1731, "died": 1781, "father": "Pieter de Decker", "mother": "Livina Haverbeke"}',
+  '{"name": "Livina Sierens", "sex": "f", "born": 1761, "died": 1826, "father": "Jan Sierens", "mother": "Maria van Waes"}',
+  '{"name": "Laurentia Haverbeke", "sex": "f", "born": 1710, "died": 1786, "father": "Jan Haverbeke", "mother": "Maria de Rycke"}',
+  '{"name": "Carel Haverbeke", "sex": "m", "born": 1796, "died": 1837, "father": "Pieter Antone Haverbeke", "mother": "Livina Sierens"}',
+  '{"name": "Elisabeth Hercke", "sex": "f", "born": 1632, "died": 1674, "father": "Willem Hercke", "mother": "Margriet de Brabander"}',
+  '{"name": "Jan Haverbeke", "sex": "m", "born": 1671, "died": 1731, "father": "Lieven Haverbeke", "mother": "Elisabeth Hercke"}',
+  '{"name": "Anna van Hecke", "sex": "f", "born": 1607, "died": 1670, "father": "Paschasius van Hecke", "mother": "Martijntken Beelaert"}',
+  '{"name": "Maria Sturm", "sex": "f", "born": 1835, "died": 1917, "father": "Charles Sturm", "mother": "Seraphina Spelier"}',
+  '{"name": "Jacobus Bernardus van Brussel", "sex": "m", "born": 1736, "died": 1809, "father": "Jan van Brussel", "mother": "Elisabeth Haverbeke"}'
+].join(",\n  ") + "\n]";
+
+// This makes sure the data is exported in node.js —
+// `require(./path/to/ancestry.js)` will get you the array.
+if (typeof module != "undefined" && module.exports)
+  module.exports = ANCESTRY_FILE;
+
+var ancestry = JSON.parse(ANCESTRY_FILE);
+
+
+// Chapter 5:  FLATTENING
+
+var arrays = [[1, 2, 3], [4, 5], [6]];
+
+function flattenArray(arr) {
+  return arr.reduce(function(res, cur) {
+  	return res.concat(cur);
+  });
+}
+
+logHeader("flattening an array of arrays");
+console.log(flattenArray(arrays));
+
+// → [1, 2, 3, 4, 5, 6]
+
+
+// Chapter 5:  MOTHER-CHILD AGE DIFFERENCE
+
+function average(array) {
+  function plus(a, b) { return a + b; }
+  return array.reduce(plus) / array.length;
+}
+
+var byName = {};
+ancestry.forEach(function(person) {
+  byName[person.name] = person;
+});
+
+
+var ageDiffs = ancestry
+
+.filter(function(person) {
+  return (person.mother && byName[person.mother] && person.born && byName[person.mother].born)
+})
+
+.map(function(person) {
+  return person.born - byName[person.mother].born;
+});
+
+logHeader("Averaging age differences");
+console.log(average(ageDiffs));
+// → 31.2
+
+
+// Chapter 5: HISTORICAL LIFE EXPECTANCY
+
+    function plus(a, b) { return a + b; }
+
+    function average(array) {
+      return array.reduce(plus) / array.length;
+    }
+
+    // FUNCTION TO GROUP AN ARRAY OF OBJECTS BASED ON THE GROUP NAME RETURNED BY A GIVEN FUNCTION
+    function groupBy(array, fn) {
+      var obj = {};
+      array.forEach(function(el) {
+        var group = fn(el);
+        if (obj[group])
+          obj[group].push( el )
+        else
+          obj[group] = [ el ]
+      });
+      return obj;
+    }
+
+    // GROUP THE ANCESTRY
+    groupedPeople = groupBy(ancestry, function(person) {
+    	return Math.ceil(person.died / 100);
+    });
+
+    var lifespans = {};
+
+    // ITERATE THROUGH GROUPS IN OBJECT AND RECORD LIFESPANS IN lifespans OBJECT
+    for (var group in groupedPeople) {
+      lifespans[group] = groupedPeople[group]
+
+      	// ONLY LOOK AT PEOPLE WHO HAVE ALREADY DIED
+      	.filter(function(person) {
+       		return person.died;
+      	})
+
+      	// GET LIFESPANS
+      	.map(function(person) {
+       	 	return person.died - person.born;
+      	})
+
+      	// CALCULATE AVERAGE
+        .reduce(plus) / groupedPeople[group].length;
+      };
+
+      logHeader("Calculating average lifespans for various centuries");
+      console.log(lifespans);
+
+    // → 16: 43.5
+    //   17: 51.2
+    //   18: 52.8
+    //   19: 54.8
+    //   20: 84.7
+    //   21: 94
+
+// Chapter 5: EVERY AND THEN SOME
+
+function every(array, fn) {
+  var bool = true;
+  for (var i = 0; i < array.length; i++) {
+
+    // IF ALL PREVIOUS ELEMENTS HAVE RETURNED TRUE,
+    // EVALUATE FUNCTION AGAIN WITH NEXT ELEMENT AS ARG
+    bool = bool && fn(array[i]);
+  }
+  return bool;
+}
+
+function some(array, fn) {
+  for (var i = 0; i < array.length; i++) {
+
+    // IF FUNCTION RETURNS TRUE, RETURN TRUE
+    if (fn(array[i]))
+      return true;
+  }
+
+  // IF NO ELEMENT EVALUATED TO TRUE, RETURN FALSE
+  return false;
+}
+
+logHeader("Rewriting 'every' and 'some' array methods");
+
+console.log(every([NaN, NaN, NaN], isNaN));
+// → true
+console.log(every([NaN, NaN, 4], isNaN));
+// → false
+console.log(some([NaN, 3, 4], isNaN));
+// → true
+console.log(some([2, 3, 4], isNaN));
+// → false
